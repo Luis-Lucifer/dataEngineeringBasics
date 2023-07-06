aa387645f8ae4fdf6ce55f82c6babd3c282bffa2
NilanjanEMinds <nilanjan.das@eminds.ai>
2023-07-06 10:10:25 +0530

    data saved to file

--- src/main/java/org/nilanjan/GetDataFromHttpEndpoint.java
+++ src/main/java/org/nilanjan/GetDataFromHttpEndpoint.java
@@ -5,18 +5,41 @@ import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class GetDataFromHttpEndpoint {
     public static void main(String[] args) throws InterruptedException {
         SparkConf conf = new SparkConf().setAppName("GetDataFromHttpEndpoint").setMaster("local[*]");
         JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
-        String endpointUrl = "https://jsonplaceholder.typicode.com/posts";
+        String endpointUrl = "https://api.publicapis.org/entries";
         JavaReceiverInputDStream<String> lines = jssc.receiverStream(new HttpReceiver(endpointUrl));
-        lines.foreachRDD(rdd -> {
-            rdd.foreach(data -> {
-                System.out.println(data);
+//        lines.foreachRDD(rdd -> {
+//            rdd.foreach(data -> {
+//                System.out.println(data);
+//            });
+//        });
+
+        lines.foreachRDD(rdd ->{
+            rdd.foreachPartition(records -> {
+                while (records.hasNext()){
+                    String record = records.next();
+                    System.out.println(record);
+                    save(record);
+                }
             });
         });
+
         jssc.start();
         jssc.awaitTermination();
+    }
+
+    public static void save(String record) throws IOException {
+        String filepath = "D:/E_minds/MyProjectWork/GetDataFrom_Http_endpoint/getDataFromURL/file.txt";
+
+        FileWriter writer = new FileWriter(filepath,true);
+        writer.write(record);
+        writer.write(System.lineSeparator());
+        writer.flush();
     }
 }
